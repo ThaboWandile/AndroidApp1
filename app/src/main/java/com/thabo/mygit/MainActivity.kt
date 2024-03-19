8e071a92af7bf44577da2e274aaae3af2c88a0a6
ThaboWandile <tmagagula83@gmail.com>
2024-03-19 14:53:41 +0200

    My first commit

---
+++ app/src/main/java/com/thabo/mygit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thabo.mygit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
